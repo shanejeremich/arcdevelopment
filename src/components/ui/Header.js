fb99b4eaa970afe736f0bd477d9c10859a36a273
Shane Jeremich <shane.jeremich@gmail.com>
2021-07-15 18:28:36 -0600

    Added logo and tabs to header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { cloneElement } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -14,14 +18,26 @@ function ElevationScroll(props) {
     threshold: 0
   });
 
-  return React.cloneElement(children, {
+  return cloneElement(children, {
     elevation: trigger ? 4 : 0
   });
 }
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }));
 
@@ -32,9 +48,17 @@ export default function Header(props) {
     <>
       <ElevationScroll>
         <AppBar position="fixed">
-          <Typography variant="h3">
-            <Toolbar>Arc Development</Toolbar>
-          </Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt="company logo" className={classes.logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button>Free Estimate</Button>
+          </Toolbar>
         </AppBar>
       </ElevationScroll>
       <div className={classes.toolbarMargin} />
