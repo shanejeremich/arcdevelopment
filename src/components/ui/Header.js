5ebe73c47f562b184e67d508cecab7bc08425e76
Shane Jeremich <shane.jeremich@gmail.com>
2021-07-15 18:36:36 -0600

    added free estimate button

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -38,6 +38,13 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px"
   }
 }));
 
@@ -57,7 +64,9 @@ export default function Header(props) {
               <Tab className={classes.tab} label="About Us" />
               <Tab className={classes.tab} label="Contact Us" />
             </Tabs>
-            <Button>Free Estimate</Button>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
